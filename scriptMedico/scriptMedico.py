3daa3caaacd33b7a0b77eec1da6bf760d26d3237
JoaocGuerra <jcarlos.guerra@hotmail.com>
2022-04-19 23:51:52 -0300

    Update all variables name and add two new functions

--- scriptMedico/scriptMedico.py
+++ scriptMedico/scriptMedico.py
@@ -1,48 +1,59 @@
-import requests
 import time
 from datetime import datetime
 
-data = datetime.today().strftime('%d-%m-%Y')
-dataURL = datetime.today().strftime('%d%m%Y')
-dataURL = dataURL[:4] + dataURL[6:]
+import requests
+
+
+def menu(data, numeropaciente):
+    print("Data: ", data)
+    print("Paciente em atendimento: ", numeropaciente)
+    print("==========================")
+    print("#OP#         Ação")
+    print("#1#  Atendimento concluído")
+    print("#2#  Encerrar aplicação")
+    print("==========================")
+
+
+def serviceCompleted(numeropaciente):
+    print("Atendimento do Paciente " + numeropaciente + " encerrado", )
+    time.sleep(1)
+    print("Chamando próximo paciente")
+    time.sleep(1)
+    print(".")
+    time.sleep(1)
+    print(".")
+    time.sleep(1)
+    print(".")
+    time.sleep(1)
+
+
+dateVIEW = datetime.today().strftime('%d-%m-%Y')
+dateURL = datetime.today().strftime('%d%m%Y')
+dateURL = dateURL[:4] + dateURL[6:]
 
 headers = {
     'Accept': '*/*',
     'User-Agent': 'request'
 }
 
-urlEA = "http://127.0.0.1:5000/atendimento/read/"
+urlOA = "http://127.0.0.1:5000/atendimento/read/"
 
-matriculaMedico = str(input("Insira sua mátricula: "))
-pacienteEmAtendimento = urlEA + matriculaMedico + "/" + dataURL
+doctorRegistration = str(input("Insira sua mátricula: "))
+patientInCare = urlOA + doctorRegistration + "/" + dateURL
 
 while 1:
-    numeroPaciente = str(requests.get(pacienteEmAtendimento).json()['paciente'])
-    print("Data: ", data)
-    print("Paciente em atendimento: ", numeroPaciente)
-    print("==========================")
-    print("#OP#         Ação")
-    print("#1#  Atendimento concluído")
-    print("#2#  Encerrar aplicação")
-    print("==========================")
-    opcao = int(input("Insira a opcão desejada: "))
-    if opcao == 1:
-        print("Atendimento do Paciente " + numeroPaciente + " encerrado", )
-        time.sleep(1)
-        print("Chamando próximo paciente")
-        proximoPaciente = {
-            "codigo_medico": matriculaMedico,
-            "dia_mes_ano": dataURL,
-            "posicao_paciente_atendido": int(numeroPaciente)
+    patientPosition = str(requests.get(patientInCare).json()['paciente'])
+    menu(dateVIEW, patientPosition)
+    option = int(input("Insira a opcão desejada: "))
+    if option == 1:
+        serviceCompleted(patientPosition)
+        nextPatient = {
+            "codigo_medico": doctorRegistration,
+            "dia_mes_ano": dateURL,
+            "posicao_paciente_atendido": int(patientPosition)
         }
         urlPP = "http://127.0.0.1:5000/atendimento/update/paciente-atendido"
-        requests.put(urlPP, headers=headers, json=proximoPaciente)
-        time.sleep(1)
-        print(".")
-        time.sleep(1)
-        print(".")
-        time.sleep(1)
-        print(".")
-        time.sleep(1)
-    elif opcao == 2:
+        requests.put(urlPP, headers=headers, json=nextPatient)
+
+    elif option == 2:
         break
